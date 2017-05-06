5da88a429fba532d22b05b9e8de49b4fb773b71a
Philip Bateman <darktweak@gmail.com>
2017-05-06 04:05:00 +0100

    Excerise 3: Added ability to delete rows
    Using the checkbox system, allows for multiple deletion entries to happen by using the studentid as a reference for the rows.

--- students.php
+++ students.php
@@ -9,12 +9,22 @@ $conn = mysqli_connect($servername,$username,$password,$dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-//Creates the SQL statement to retrieve the data from the db via the conn (connection)
+
+//Handling for deleting
+//upon sumbiting the input DeleteForms it checks to see if it still exists
+if(isset($_POST['selectedRows'])) {
+	// searches through array for any of the studentid numbers
+	foreach($_POST['selectedRows'] as $row) {
+		// deletes the id's that were in the assoc array and then escapes from deletion.
+		$conn->query("DELETE FROM student WHERE studentid = '" . mysqli_real_escape_string($conn, $row) . "'");
+	}
+}
+
+
+// Creates the SQL statement to retrieve the data from the db via the conn (connection)
 $sql = "SELECT studentid, dob, firstname, lastname, house, town, county, country, postcode FROM student";
 $result = $conn->query($sql);
-// closes the connection if a connection wants to be created elsewhere
-$conn->close();
-//including the styling templates
+// including the styling templates
 include('templates/partials/header.php');
 include('templates/partials/nav.php');
 ?>
@@ -24,36 +34,44 @@ include('templates/partials/nav.php');
 </head>
 <body>
 	<div class="container">
-		<table class="table table-striped">
-			<!-- table header -->
-			<thead>
-				<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>
-			</thead>
-			<!-- table body -->
-			<tbody>
-				<?php
-				if ($result->num_rows > 0) {
-// output data of each row
-					while($row = $result->fetch_assoc()) {
-						echo "<tr><td>". $row["studentid"]. 
-						"</td><td>". $row["firstname"]. 
-						"</td><td>". $row["lastname"]. 
-						"</td><td>". $row["house"]. 
-						"</td><td>". $row["town"]. 
-						"</td><td>". $row["county"].
-						"</td><td>". $row["country"]. 
-						"</td><td>". $row["postcode"].
-						"</td><td><input type=\"checkbox\" value=\"".$row["studentid"]."\"></td>".
-						"</td></tr>";
+	<!-- Creation of form for the table functionality I need -->
+		<form action="students.php" method="post">
+			<table class="table table-striped">
+				<!-- table header -->
+				<thead>
+					<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>
+				</thead>
+				<!-- table body -->
+				<tbody>
+					<?php
+					if ($result->num_rows > 0) {
+	// output data of each row
+						while($row = $result->fetch_assoc()) {
+							echo "<tr><td>". $row["studentid"]. 
+							"</td><td>". $row["firstname"]. 
+							"</td><td>". $row["lastname"]. 
+							"</td><td>". $row["house"]. 
+							"</td><td>". $row["town"]. 
+							"</td><td>". $row["county"].
+							"</td><td>". $row["country"]. 
+							"</td><td>". $row["postcode"].
+							"</td><td><input type=\"checkbox\" name=\"selectedRows[]\" value=\"".$row["studentid"]."\"></td>".
+							"</td></tr>";
+						}
+					} else {
+						echo "0 results";
+	// print_r($result);
 					}
-				} else {
-					echo "0 results";
-// print_r($result);
-				}
-				?>
-			</tbody>
-		</table>
+					?>
+				</tbody>
+			</table>
+			<input type="submit" name="DeleteForms" class="btn btn-danger pull-right" value='Delete'/>
+		</form>
 	</div>
 </div>
+<?php
+// closes the connection if a connection wants to be created elsewhere
+$conn->close();
+?>
 </body>
 </html>
